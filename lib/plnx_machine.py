3e691e28bf47876fb7e0c4be3c62be6b9d46bf87
Lomate, Ashwini <ashwini.lomate@amd.com>
2024-10-30 00:38:26 -1200

    plnx_machine.py: Remote ATF not picking custom branch and commit Id with petalinux 2024.2
    As per the recent changes in YP, variables SRC_URI_TRUSTED_FIRMWARE_A, SRCREV_tfa and SRCBRANCH name got changed.
    So add same variable name in petalinux
    
    Signed-off-by: Ashwini Lomate <ashwini.lomate@amd.com>

--- lib/plnx_machine.py
+++ lib/plnx_machine.py
@@ -29,7 +29,7 @@ def AddRemoteSources(component, Kcomponent):
     conf_prop = {
         'linux-xlnx': ['KERNELURI', 'SRCREV', 'KBRANCH', 'LIC_FILES_CHKSUM'],
         'u-boot-xlnx': ['UBOOTURI', 'SRCREV', 'UBRANCH', 'LIC_FILES_CHKSUM'],
-        'trusted-firmware-a': ['REPO', 'SRCREV', 'BRANCH', 'LIC_FILES_CHKSUM'],
+        'trusted-firmware-a': ['SRC_URI_TRUSTED_FIRMWARE_A', 'SRCREV_tfa', 'SRCBRANCH', 'LIC_FILES_CHKSUM'],
         'plm-firmware': ['REPO', 'SRCREV', 'BRANCH', 'LIC_FILES_CHKSUM'],
         'psm-firmware': ['REPO', 'SRCREV', 'BRANCH', 'LIC_FILES_CHKSUM'],
     }
